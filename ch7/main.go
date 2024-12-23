1c63d0931bde2c96342c20af6f2bea75dd03355f
dijonron <dronan@ualberta.ca>
2024-12-23 14:34:51 -0700

    remove ex. comment block

--- ch7/main.go
+++ ch7/main.go
@@ -76,11 +76,6 @@ func NewLeague(name string, teamNames []string) League {
 	}
 }
 
-// Define an interface called Ranker that has a single method called Ranking that returns a slice of strings.
-// Write a function called RankPrinter with two parameters, the first of type Ranker and the second of type io.Writer.
-// Use the io.WriteString function to write the values returned by Ranker to the io.Writer, with a newline separating each result.
-// Call this function from main.
-
 type Ranker interface {
 	Ranking() []string
 }
